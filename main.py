5b214add481847cd0224500ca101d469fde5f46d
Rouven Bissinger <info@rouven.xyz>
2022-06-30 19:39:13 +0200

    add time messurement

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from time import time
 import rsa
 
 if __name__ == "__main__":
-    rsa.mul_inv(17,6)
     k = rsa.genKeyPair()
     print(k)
     t = 15
+    z = time()
     c = (t**k.public) % k.modulus
+    print("time encrypt:", time()-z)
+    z = time()
     t2 = (c**k.private) % k.modulus
+    print("time:", time()-z)
     print(t)
     print(c)
     print(t2)
