f10473a91c749e547fea08f2eb659270214e1e45
Rouven Bissinger <info@rouven.xyz>
2022-06-30 15:27:08 +0200

    improvements
    very improved!
    
    Co-Authored-By: Sebastian <contact@suttersebastian.com>

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import rsa
 
 if __name__ == "__main__":
-    pass
+    print(rsa.genKeyPair())
--- rsa.py
+++ rsa.py
@@ -1,31 +1,38 @@
+import imp
 import random
+import math
+
 
 def isPrime(x):
     """returns True if x is prime"""
-    for i in range(2, int(x**0.5)+1):
+    for i in range(2, int(math.sqrt(x)+1)):
         if x % i == 0:
             return False
     return True
 
-    pass
 
 def genPrimeOfSize(min, max):
     """returns a large pseudorandom prime number as int or -1"""
     if min == max:
-        print("no prime found in given range")
+        print("no prime found in given range/ range to small")
         return -1
     rand = random.randint(min, max) # random number in given range
-    x = random.choice((-1, +1)) # going up or donwarts
     prime = rand
-    
-    searching the prime number
+
+    if prime % 2 == 0:
+        prime += 1
+    # searching the prime number
     while not isPrime(prime):
-        prime += x
+        prime += 2
         if prime > max:
-            genPrimeOfSize(min, rand);
-        elif prime < min:
-            genPrimeOfSize(rand, max)
+            genPrimeOfSize(min, rand)
 
     return prime
 
 
+def genKeyPair():
+    p = genPrimeOfSize( 2**10,2**11)
+    q = genPrimeOfSize(2**10, 2**11)
+    n = p * q
+    
+    return p,q
